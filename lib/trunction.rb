a3a710235b01c4b24404bc1379f7da02f0a6cb30
Mike Williams <mdub@dogbiscuit.org>
2011-11-26 22:19:12 +1100

    Passing test; ugly code.

--- lib/trunction.rb
+++ lib/trunction.rb
@@ -6,7 +6,17 @@ module Trunction
   extend self
 
   def truncate_html(html, min, max = min)
-    html
+    doc = Nokogiri::HTML::DocumentFragment.parse(html)
+    length = 0
+    doc.children.each do |child|
+      child_length = child.text.length
+      if length + child_length > max
+        child.remove
+      else
+        length += child_length
+      end
+    end
+    doc.to_html
   end
 
 end
--- spec/trunction_spec.rb
+++ spec/trunction_spec.rb
@@ -2,19 +2,47 @@ require 'trunction'
 
 describe Trunction do
 
-  let(:p) { ["paragraph one", "paragraph two"] }
-  let(:input) { p.map { |text| "<p>#{text}</p>" }.join("\n") }
+  let(:paragraphs) { ["paragraph one", "paragraph two"] }
+  let(:input) { paragraphs.map { |text| p(text) }.join }
   let(:total_length) { Nokogiri::HTML::DocumentFragment.parse(input).text.length }
   let(:ellipsis) { "&#8230;" }
+
+  def p(text)
+    "<p>#{text}</p>"
+  end
 
   include Trunction
 
   describe "#truncate_html" do
 
-    context "with limit longer than input" do
+    let(:min_length) { 1 }
+
+    let(:result) do
+      truncate_html(input, min_length, max_length)
+    end
+
+    context "with max-length longer than input" do
+
+      let(:max_length) { total_length + 1 }
 
       it "returns input intact" do
-        truncate_html(input, total_length + 1).should == input
+        result.should == input
+      end
+
+    end
+
+    context "with max-length shorter than input" do
+
+      let(:max_length) { total_length - 1 }
+
+      context "and liberal min-length" do
+
+        let(:min_length) { total_length - 1 }
+
+        it "drops elements until beneath max-length" do
+          result.should == p(paragraphs.first)
+        end
+
       end
 
     end
