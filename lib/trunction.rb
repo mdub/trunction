134911acfa9ef13d9dd63e8abbeb50db021a72bd
Mike Williams <mdub@dogbiscuit.org>
2011-11-26 22:41:57 +1100

    No prettier, but slightly better.

--- lib/trunction.rb
+++ lib/trunction.rb
@@ -7,15 +7,15 @@ module Trunction
 
   def truncate_html(html, min, max = min)
     doc = Nokogiri::HTML::DocumentFragment.parse(html)
-    length = 0
-    doc.children.each do |child|
-      child_length = child.text.length
-      if length + child_length > max
-        child.remove
-      else
-        length += child_length
-      end
+    chars_remaining = max
+    last = 0
+    doc.children.each_with_index do |node, i|
+      node_length = node.text.length
+      break if node_length > chars_remaining
+      chars_remaining -= node_length
+      last = i
     end
+    doc.children[(last + 1)..-1].remove
     doc.to_html
   end
 
