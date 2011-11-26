2a42566c014dc75f601e9543d1666464cd5705c9
Mike Williams <mdub@dogbiscuit.org>
2011-11-27 08:39:56 +1100

    Tweak spec.

--- spec/trunction_spec.rb
+++ spec/trunction_spec.rb
@@ -2,45 +2,49 @@ require 'trunction'
 
 describe Trunction do
 
-  let(:paragraphs) { ["paragraph one", "paragraph two"] }
-  let(:input) { paragraphs.map { |text| p(text) }.join }
+  let(:paragraphs) { ["paragraph one", "paragraph two", "paragraph three"] }
+  let(:input) { p(*paragraphs) }
   let(:total_length) { Nokogiri::HTML::DocumentFragment.parse(input).text.length }
   let(:ellipsis) { "&#8230;" }
 
-  def p(text)
-    "<p>#{text}</p>"
+  def p(*paragraphs)
+    paragraphs.map { |text| "<p>#{text}</p>" }.join
   end
 
   include Trunction
 
-  describe "#truncate_html" do
+  context "given multiple paragraphs" do
 
-    let(:min_length) { 1 }
+    describe "#truncate_html" do
 
-    let(:result) do
-      truncate_html(input, min_length, max_length)
-    end
+      let(:min_length) { 1 }
 
-    context "with max-length longer than input" do
-
-      let(:max_length) { total_length + 1 }
-
-      it "returns input intact" do
-        result.should == input
+      let(:result) do
+        truncate_html(input, min_length, max_length)
       end
 
-    end
+      context "with max-length longer than input" do
 
-    context "with max-length shorter than input" do
+        let(:max_length) { total_length + 1 }
 
-      let(:max_length) { total_length - 1 }
+        it "returns input intact" do
+          result.should == input
+        end
 
-      context "and liberal min-length" do
+      end
 
-        let(:min_length) { total_length - 1 }
+      context "with max-length shorter than input" do
 
-        it "drops elements until beneath max-length" do
-          result.should == p(paragraphs.first)
+        let(:max_length) { total_length - 1 }
+
+        context "and liberal min-length" do
+
+          let(:min_length) { 0 }
+
+          it "drops elements until beneath max-length" do
+            result.should == p(*paragraphs[0,2])
+          end
+
         end
 
       end
