4b146fc9675a049646d2cf7431252ff3ed988094
Mike Williams <mdub@dogbiscuit.org>
2011-11-27 13:13:24 +1100

    Test truncation inside tags.

--- spec/trunction_spec.rb
+++ spec/trunction_spec.rb
@@ -2,26 +2,22 @@ require 'trunction'
 
 describe Trunction do
 
-  let(:paragraphs) { ["paragraph one", "paragraph two", "paragraph three"] }
-  let(:input) { p(*paragraphs) }
   let(:total_length) { Nokogiri::HTML::DocumentFragment.parse(input).text.length }
   let(:ellipsis) { "&#8230;" }
 
-  def p(*paragraphs)
-    paragraphs.map { |text| "<p>#{text}</p>" }.join
-  end
-
   include Trunction
+
+  let(:min_length) { 1 }
+
+  let(:result) do
+    truncate_html(input, min_length, max_length).gsub("\n", '')
+  end
 
   context "given multiple paragraphs" do
 
+    let(:input) { "<p>one</p><p>two</p><p>three</p>" }
+
     describe "#truncate_html" do
-
-      let(:min_length) { 1 }
-
-      let(:result) do
-        truncate_html(input, min_length, max_length)
-      end
 
       context "with max-length longer than input" do
 
@@ -37,16 +33,26 @@ describe Trunction do
 
         let(:max_length) { total_length - 1 }
 
-        context "and liberal min-length" do
-
-          let(:min_length) { 0 }
-
-          it "drops elements until beneath max-length" do
-            result.should == p(*paragraphs[0,2])
-          end
-
+        it "drops elements until beneath max-length" do
+          result.should == "<p>one</p><p>two</p>"
         end
 
+      end
+
+    end
+
+  end
+
+  context "given multiple paragraphs, wrapped in a div" do
+
+    let(:input) { "<div><p>one</p><p>two</p><p>three</p></div>" }
+
+    context "with max-length shorter than input" do
+
+      let(:max_length) { total_length - 1 }
+
+      it "drops elements until beneath max-length" do
+        result.should == "<div><p>one</p><p>two</p></div>"
       end
 
     end
