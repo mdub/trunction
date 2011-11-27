dbb075bfc73aae023ffb35f720ba13610c7969ac
Mike Williams <mdub@dogbiscuit.org>
2011-11-27 21:50:33 +1100

    Drop block elements, if possible.

--- lib/trunction.rb
+++ lib/trunction.rb
@@ -14,16 +14,18 @@ module Trunction
   private
 
   def last_allowable_element_in(doc, max)
-    last_element = doc
+    last_element = nil
+    last_block_element = nil
     chars_remaining = max
     doc.traverse do |node|
       if node.text?
         chars_remaining -= node.text.length
         break unless chars_remaining > 0
       end
+      last_block_element = node if node.description && node.description.block?
       last_element = node
     end
-    last_element
+    last_block_element || last_element
   end
 
   def remove_everything_after(node)
--- spec/trunction_spec.rb
+++ spec/trunction_spec.rb
@@ -59,4 +59,20 @@ describe Trunction do
 
   end
 
+  context "a final paragraph with inline elements" do
+
+    let(:input) { "<p>one</p><p>two</p><p><b>one</b>, <b>two</b>, <b>three</b>, <b>four</b></p>" }
+
+    context "with max-length shorter than input" do
+
+      let(:max_length) { total_length - 1 }
+
+      it "drops the entire final paragraph" do
+        result.should == "<p>one</p><p>two</p>"
+      end
+
+    end
+
+  end
+
 end
