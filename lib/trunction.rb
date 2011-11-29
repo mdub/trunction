c263cc41348df890a8c47068a048b3ba2937e2bc
Mike Williams <mdub@dogbiscuit.org>
2011-11-29 22:31:21 +1100

    Go all object-oriented.

--- lib/trunction.rb
+++ lib/trunction.rb
@@ -7,33 +7,53 @@ module Trunction
 
   def truncate_html(html, max)
     doc = Nokogiri::HTML::DocumentFragment.parse(html)
-    remove_everything_after(last_allowable_element_in(doc, max))
+    Truncation.new(doc, max).execute
     doc.to_html
   end
 
-  private
+  class Truncation
 
-  def last_allowable_element_in(doc, max)
-    last_element = nil
-    last_block_element = nil
-    chars_remaining = max
-    doc.traverse do |node|
-      if node.text?
-        chars_remaining -= node.text.length
-        break unless chars_remaining > 0
+    def initialize(doc, max)
+      @doc = doc
+      @max = max
+      @chars_remaining = max
+    end
+
+    def execute
+      find_the_end
+      remove_everything_else
+    end
+
+    private
+
+    def find_the_end
+      catch(:done) do
+        @doc.traverse do |node|
+          accumulate_text(node) if node.text?
+          @last_block_element = node if node.description && node.description.block?
+          @last_element = node
+        end
       end
-      last_block_element = node if node.description && node.description.block?
-      last_element = node
     end
-    last_block_element || last_element
-  end
 
-  def remove_everything_after(node)
-    while node
-      node.next.remove while node.next
-      break unless node.respond_to?(:parent)
-      node = node.parent
+    def accumulate_text(text_node)
+      @chars_remaining -= text_node.text.length
+      throw(:done) unless @chars_remaining > 0
     end
+
+    def last_node
+      @last_block_element || @last_element
+    end
+
+    def remove_everything_else
+      node = last_node
+      while node
+        node.next.remove while node.next
+        break unless node.respond_to?(:parent)
+        node = node.parent
+      end
+    end
+
   end
 
 end
